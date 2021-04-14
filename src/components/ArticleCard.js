b011bd9fb6983ebee677fcbda11cbaf91b006ab4
Suhas Jagannath <sjaga003@ucr.edu>
2021-04-13 22:05:38 -0700

    Articles mobile css

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ArticleCard = ({ cardInfo }) => {
   return (
-    <div>
+    <div className="bg-white mb-6 rounded-md overflow-hidden">
       <img src={cardInfo.image} alt={cardInfo.title} />
-      <div>
-        <span>By {cardInfo.author}</span>
-        <span>{cardInfo.title}</span>
-        <span>{cardInfo.content}</span>
+      <div className="p-4 flex flex-col">
+        <span className="text-secondary-300 text-sm">By {cardInfo.author}</span>
+        <span className="text-primary-0 py-2">{cardInfo.title}</span>
+        <span className="text-sm text-secondary-300">{cardInfo.content}</span>
       </div>
     </div>
   );
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import plane from '../img/image-plane.jpg';
+import restaurant from '../img/image-restaurant.jpg';
+import confetti from '../img/image-confetti.jpg';
 import currency from '../img/image-currency.jpg';
 import ArticleCard from './ArticleCard';
 
@@ -11,19 +13,19 @@ const articleInfo = [
     content: `The world is getting smaller and we're becoming more mobile. So why should you be forced to only recieve money in a single...`,
   },
   {
-    image: currency,
+    image: restaurant,
     author: 'Wilson Hutton',
     title: 'Treat yourself without worrying about money',
     content: `Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you...`,
   },
   {
-    image: currency,
+    image: plane,
     author: 'Wilson Hutton',
     title: 'Take your coolBank card wherever you go',
     content: `We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you...`,
   },
   {
-    image: currency,
+    image: confetti,
     author: 'Claire Robinson',
     title: 'Our invite-only Beta accounts are live!',
     content: `After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site...`,
@@ -32,11 +34,13 @@ const articleInfo = [
 
 const Articles = () => {
   return (
-    <section>
-      <span>Latest Articles</span>
-      {articleInfo.map((card) => (
-        <ArticleCard cardInfo={card} />
-      ))}
+    <section className="py-16 flex flex-col items-center bg-secondary-100 overflow-hidden rounded-md">
+      <span className="text-primary-0 text-3xl pb-4">Latest Articles</span>
+      <div className="px-4">
+        {articleInfo.map((card) => (
+          <ArticleCard cardInfo={card} />
+        ))}
+      </div>
     </section>
   );
 };
